80deacc95cd38a2874d09b7bbc1081dfad4e225f
acebollero <alfonso.cebollero@axians.es>
2022-09-03 17:08:00 +0200

    ADD omitempty flag to users entity struct

--- entities/users.go
+++ entities/users.go
@@ -6,13 +6,13 @@ import (
 )
 
 type User struct {
-	Id        primitive.ObjectID `bson:"_id"`
-	FirstName string             `bson:"first_name"`
-	LastName  string             `bson:"last_name"`
-	Nickname  string             `bson:"nickname"`
-	Password  string             `bson:"password"`
-	Email     string             `bson:"email"`
-	Country   string             `bson:"country"`
-	CreatedAt time.Time          `bson:"created_at"`
-	UpdatedAt time.Time          `bson:"updated_at"`
+	Id        primitive.ObjectID `bson:"_id,omitempty"`
+	FirstName string             `bson:"first_name,omitempty"`
+	LastName  string             `bson:"last_name,omitempty"`
+	Nickname  string             `bson:"nickname,omitempty"`
+	Password  string             `bson:"password,omitempty"`
+	Email     string             `bson:"email,omitempty"`
+	Country   string             `bson:"country,omitempty"`
+	CreatedAt time.Time          `bson:"created_at,omitempty"`
+	UpdatedAt time.Time          `bson:"updated_at,omitempty"`
 }
