88218ee749b985badf3e66f56faeb7d30ec82e6a
acebollero <alfonso.cebollero@axians.es>
2022-09-03 17:06:38 +0200

    ADD new errors

--- entities/errors.go
+++ entities/errors.go
@@ -3,5 +3,7 @@ package entities
 import "google.golang.org/grpc/status"
 
 var (
+	InvalidEmailError           = status.Error(3, "entered email is not valid")
 	AlreadyRegisteredEmailError = status.Error(6, "email already registered")
+	NotFoundUser                = status.Error(5, "could not found user")
 )
