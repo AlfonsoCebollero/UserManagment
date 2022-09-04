083475342ff64b4dd277213815c465ed377149e7
acebollero <alfonso.cebollero@axians.es>
2022-09-04 14:11:37 +0200

    ADD notifications handling and server side streaming method
    ADD comments

--- infra/server/server.go
+++ infra/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"userManagement/infra/database"
 	pb "userManagement/proto"
@@ -9,9 +10,12 @@ import (
 
 type UserManagementServer struct {
 	pb.UnimplementedUserManagementServer
-	DbClient database.AdapterInterface
+	DbClient      database.AdapterInterface
+	NotifyChannel chan []string
 }
 
+// CreateUser creates a new user from the received request and returns user details
+// It sends a creation action notification
 func (s *UserManagementServer) CreateUser(ctx context.Context, in *pb.CreateUserReq) (*pb.UserActionResponse, error) {
 	user := in.GetUser()
 	log.Printf("Received: %v", user)
@@ -30,27 +34,36 @@ func (s *UserManagementServer) CreateUser(ctx context.Context, in *pb.CreateUser
 		return nil, err
 	}
 
+	s.notify(in.User.String(), "Created")
 	return createdUser, nil
 }
 
+// GetUser retrieves a user by id or by email
+// it sends a retrieving action notification
 func (s *UserManagementServer) GetUser(ctx context.Context, in *pb.GetUserReq) (*pb.UserActionResponse, error) {
 	user, err := s.DbClient.GetUser(in)
 	if err != nil {
 		return nil, err
 	}
 
+	s.notify(in.UserId, "Retrieved")
 	return user, nil
 }
 
+// UpdateUser updates a user, who is found by email or ID. It uses the body of the request to update.
+// It sends an update action notification.
 func (s *UserManagementServer) UpdateUser(ctx context.Context, in *pb.UpdateUserReq) (*pb.UserActionResponse, error) {
 	user, err := s.DbClient.UpdateUser(in)
 	if err != nil {
 		return nil, err
 	}
 
+	s.notify(in.UserId, "Updated")
 	return user, nil
 }
 
+// DeleteUser removes a user from the database by ID or email
+// Sends a deletion action notification
 func (s *UserManagementServer) DeleteUser(ctx context.Context, in *pb.DeleteUserReq) (*pb.DeletionActionResponse, error) {
 	_, err := s.DbClient.DeleteUser(in)
 
@@ -58,9 +71,11 @@ func (s *UserManagementServer) DeleteUser(ctx context.Context, in *pb.DeleteUser
 		return &pb.DeletionActionResponse{Deleted: false}, err
 	}
 
+	s.notify(in.UserId, "Deleted")
 	return &pb.DeletionActionResponse{Deleted: true}, nil
 }
 
+// ListUsers retrieves all stored users
 func (s *UserManagementServer) ListUsers(ctx context.Context, in *pb.ListUsersReq) (*pb.ListActionResponse, error) {
 	users, err := s.DbClient.GetAllUsers(in)
 	if err != nil {
@@ -68,4 +83,28 @@ func (s *UserManagementServer) ListUsers(ctx context.Context, in *pb.ListUsersRe
 	}
 
 	return users, nil
+}
+
+// NotifyUserChanges creates a stream where action notifications are received.
+func (s *UserManagementServer) NotifyUserChanges(msg *pb.EmptyMsg, server pb.UserManagement_NotifyUserChangesServer) error {
+	for {
+		select {
+		case n := <-s.NotifyChannel:
+			notification := pb.UserActionStream{}
+			action := fmt.Sprintf("User action performed: %s - was %s", n[0], n[1])
+			notification.Action = action
+
+			err := server.Send(&notification)
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
+
+// notify sends a notification through the server channel to later be sent through
+// a server side streaming
+func (s *UserManagementServer) notify(userEmail, action string) {
+	notification := []string{userEmail, action}
+	s.NotifyChannel <- notification
 }
